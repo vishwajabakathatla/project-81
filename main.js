f10336931b6ad69d82905527ddd00d8f57d4ce89
vishwajabakathatla <88708444+vishwajabakathatla@users.noreply.github.com>
2021-08-23 12:59:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-var canvas = document.getElementById("mycanvas");
-var ctx = canvas.getContext("2d");
+anvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
 ctx.beginPath();
-ctx.strokeStyle = "pink";
-ctx.lineWidth = 2;
-ctx.arc(700, 200, 40, 0, 2 * Math.PI);
+ctx.strokeStyle = "gray";
+ctx.lineWidth = 3;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 3;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 3;
+ctx.arc(300, 250, 40, 0, 20* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 3;
+ctx.arc(355, 210, 40, 0, 20* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 3;
+ctx.arc(410, 250, 40, 0, 20* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 3;
+ctx.arc(470, 210, 40, 0, 20* Math.PI);
 ctx.stroke();
